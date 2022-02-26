ff5145ee35f9b71f29947023bac099331a3da6a6
uday <vtech321@yahoo.co.uk>
2022-02-26 14:36:24 +0000

    updated array same length fix

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def lambda_handler(event, context):
-    d={'col1':[1,2],'col2':[3,4,5]}
+    d={'col1':[1,2,3],'col2':[3,4,5]}
     df=pd.DataFrame(data=d)
     print(df)
     print('done x1.1')
